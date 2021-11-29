2ea8f47f89dfc404993f1b235fbb4499be0e35cf
zhangyuxuan <zhangyuxuan01@gmail.com>
2021-11-29 17:01:07 +0800

    增加功能 添加样式

--- src/view/arrange/Create.vue
+++ src/view/arrange/Create.vue
@@ -3,47 +3,81 @@
     <div style='position:absolute; z-index:9999' >
       <FullscreenOutlined/>
     </div>
-    <n-layout-sider>
-      <draggable
-        
+    <n-layout-sider
+      :collapsed='listsCollapsed'
+      collapse-mode='width'
+      collapsed-width='50'
+      @collapse="listsCollapsed = true"
+			@expand="listsCollapsed = false"
+    >
+
+    <div v-if='listsCollapsed === false'>
+      <div class='deep-blue' style='height: 45px'>
+        <n-icon size="25" @click='listsCollapsedHandle'>
+          <MenuFoldOutlined/>
+        </n-icon>
+        <span>语句列表</span>
+      </div>
+      <draggable 
         :list='list1'
         :group="{ name: 'list', pull: 'clone', put: false }"
       >
-        <template #item='{ element }'>
-          <n-card>
-            {{element.name}}
-          </n-card>
+      <template #item='{ element }'>
+        <n-card>
+          {{element.name}}
+        </n-card>
         </template>
-      </draggable>
+      </draggable>    
+    </div>
+    <div v-else></div>
+ 
     </n-layout-sider>
     <n-layout-content position="absolute" class="arrange-bg p-4" style="left:272px;">
+      <!-- 场景名称 -->
       <NInput style="width:350px"/>
-      <div class='bg-green-400 h-8 w-96 rounded-full border-2 border-black' style="line-height: 28px;padding-left: 25px;">
+      <!-- 场景名称End -->
+      
+      <!-- 开始标题 -->
+      <div class='bg-green-400 h-8 rounded-full border-2 border-black' style="line-height: 28px;padding-left: 25px;">
         <span>开始</span>
         <span>场景执行最大限时</span>
         <input/>
         <span>分钟</span>
       </div>
-      <draggable
-        class = 'w-96'
-        :list = 'list2'
-        :group = '{name: "list"}'
-      >
-        <template #item="{ element }">
-          <NCard>
-            {{element.name}}
-          </NCard>
-        </template>
-      </draggable>
+      <div style='height:20px; margin-left: 75px; width:2px; overflow: hidden; background-color:#aaa'></div>
+      <div style='width:0px; height:0px; border-right:6px soild transparent; border-left:6px soild transparent ;border-top:6px soild #aaa'></div>
+      <!-- 开始标题 End-->
+
+      <!-- 拖拽语句区 -->
+      <div class='placeholder'>
+        <draggable :list='list2' :group='{name: "list"}'>
+          <template #item="element">{{element.name}}</template>
+        </draggable>
+      </div>
+      <div style='height:20px; margin-left: 75px; width:2px; overflow: hidden; background-color:#aaa'></div>
+      <div style='width:0px; height:0px; border-right:6px soild transparent; border-left:6px soild transparent ;border-top:6px soild #aaa'></div>
+
+      <!-- 拖拽语句区 -->
+
+      <div class='bg-green-400 h-8 rounded-full border-2 border-black' style="line-height: 28px;padding-left: 25px;" >
+        结束
+      </div>
     </n-layout-content>  
 
   </n-layout>
 </template>
 
 <script lang="ts" setup>
-import { reactive } from 'vue'
-import { FullscreenOutlined } from '@vicons/antd'
+import { reactive, ref } from 'vue'
+import { FullscreenOutlined, MenuFoldOutlined } from '@vicons/antd'
 import draggable from 'vuedraggable'
+
+const listsCollapsed = ref(false)
+function listsCollapsedHandle () {
+	listsCollapsed.value = !listsCollapsed.value
+}
+
+
 
 const list1 = reactive([
 	{ name: 'John', id: 1 },
@@ -65,4 +99,14 @@ const list2 = reactive([
 
 }
 
+
+.placeholder {
+  width: 200px;
+  border: 1px dotted #aaa;
+  height: 30px;
+  line-height: 30px;
+  text-align: center;
+  border-radius:12px;
+
+}
 </style>
--- src/view/layout/index.vue
+++ src/view/layout/index.vue
@@ -1,27 +1,44 @@
 <template>
   <div style="height: 100vh; position: relative">
-    <n-layout position="absolute">
+    <n-layout position="absolute" :theme="darkTheme">
       <n-layout-header bordered> 
 				<Header></Header>
 			</n-layout-header>
-      <n-layout has-sider position="absolute" style="top: 64px;">
+      <n-layout has-sider position="absolute" style="top: 57px;">
         <n-layout-sider 
-					class="siderbar"
+					v-show='sideHidden===false'
+					class="siderbar text-white font-extrabold"
 					bordered
 					collapse-mode="width"
 					:collapsed-width="64"
-					:collapsed="SideCollapsed"
-					show-trigger
-					@collapse="SideCollapsed = true"
-					@expand="SideCollapsed = false"
-				>
+					:collapsed="sideCollapsed"
+					@collapse="sideCollapsed = true"
+					@expand="sideCollapsed = false"
+				> 
+					<div class='flex flex-col items-center'>
+						<n-avatar></n-avatar>
+						<div>
+							<span>许可有效期</span>
+							<span>2555-55-55</span>
+						</div>
+					</div>
 					<NMenu
+						collapsed-icon-size='34'
+						collapsed-width='63'
 						:options='menuOptions'
 					/>
+					<div class='absolute bottom-0 w-full hover:bg-red-700' style="height: 50px;" @click="collapsedhandle">
+						<n-icon class="float-right" style="line-height: 50px;" size='30'>
+							<ArrowLeftOutlined/>
+						</n-icon>
+					</div>
 				</n-layout-sider>
         <n-layout class="h-full">
 					<n-layout-header bordered class="h-12">  
 						<n-breadcrumb>
+							<n-icon v-show='sideHidden' class="text-black " size='40' @click="sideshowHandle" >
+								<DoubleRightOutlined />
+							</n-icon>
 							<n-breadcrumb-item> 这里是面包屑	</n-breadcrumb-item>
 						</n-breadcrumb>
 					</n-layout-header>
@@ -42,17 +59,56 @@ import { defineComponent, h } from 'vue'
 import Header from './components/header'
 import Sider from './components/sider'
 
-import type {MenuOption }from 'naive-ui'
+import { NIcon } from 'naive-ui'
+import type { MenuOption }from 'naive-ui'
+import { darkTheme } from 'naive-ui'
 
-const SideCollapsed = ref(true)
+import { EditFilled, SettingFilled, CaretRightFilled, ArrowLeftOutlined, DoubleRightOutlined } from '@vicons/antd'
+
+
+
+
+
+
+
+
+function renderIcon( icon: any) {
+	return () => h(NIcon, null, { default: () => h(icon) })
+}
 
 const menuOptions: MenuOption[] = [
 	{
 		label: '场景执行',
 		key: '1',
+		icon: renderIcon(CaretRightFilled),
 		children:[{label: '场景清单',},{label: '历史清单'}]
 	},
+	{
+		label: '场景编排',
+		key: '2',
+		icon: renderIcon(EditFilled),
+		children:[{label: '新建场景'},{label: '我的场景'},{label: '场景审核'}]
+	},
+	{
+		label: '系统管理',
+		key: '3',
+		icon: renderIcon(SettingFilled),
+		children:[{label: '模板管理'}, {label: '语句管理'} ,{label: '系统配置'}]
+	}
 ]
+
+
+const sideCollapsed = ref(false)
+const sideHidden = ref(false)
+function collapsedhandle () {
+	if (!sideCollapsed.value) {sideCollapsed.value = !sideCollapsed.value} else if (!sideHidden.value) { sideHidden.value = !sideHidden.value}
+}
+function sideshowHandle () {
+	sideCollapsed.value = !sideCollapsed.value
+	sideHidden.value = ! sideHidden.value 
+}
+
+
 
 </script>
 
